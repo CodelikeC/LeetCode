c513a809195af88f672182efd56f27a9c302107e
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-06-11 12:07:30 +0700

    Update PathSumII.py

--- PathSum/PathSumII.py
+++ PathSum/PathSumII.py
@@ -1,1 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        rst = []
+        self._dfs(root, sum, rst, [])
+        return rst 
+
+    def _dfs(self, root, sum, rst, path):
+        if not root:
+            return 
+
+        # add current root's value to the path 
+        path.append(root.val)
+
+        # in case this is a leaf node 
+        if not root.left and not root.right:
+            if not sum - root.val:
+                # for primitive values, [:] is sufficient (although it is doing shallow copy)
+                rst.append(path[:])
+        else:
+            self._dfs(root.left, sum - root.val, rst, path)
+            self._dfs(root.right, sum - root.val, rst, path)
+
+        # backtrack 
+        path.pop()
